967067dca8e1f8db66c35d4da8d3cefb87d3c39c
Cole Sullivan <47622215+cole-sullivan@users.noreply.github.com>
2025-05-07 07:55:11 +0000

    first iteration on battery widget

--- .local/src/fabric/main.py
+++ .local/src/fabric/main.py
@@ -6,6 +6,7 @@ from fabric.widgets.overlay import Overlay
 from fabric.widgets.eventbox import EventBox
 from fabric.widgets.datetime import DateTime
 from fabric.widgets.centerbox import CenterBox
+from fabric.core.fabricator import Fabricator
 from fabric.system_tray.widgets import SystemTray
 from fabric.widgets.wayland import WaylandWindow as Window
 from fabric.hyprland.widgets import ActiveWindow, Workspaces, WorkspaceButton
@@ -15,6 +16,71 @@ from fabric.utils import (
     invoke_repeater,
     get_relative_path,
 )
+from gi.repository import GLib, Gdk
+
+class BatteryProvider:
+    def __init__(self):
+        battery = psutil.sensors_battery()
+        if battery is None:
+            self.percent = 0.0
+            self.charging = None
+        else:
+            self.percent = battery.percent
+            self.charging = battery.power_plugged
+        
+        GLib.timeout_add_seconds(1, self._update)
+
+    def _update(self):
+        battery = psutil.sensors_battery()
+        if battery is None:
+            self.percent = 0.0
+            self.charging = None
+        else:
+            self.percent = battery.percent
+            self.charging = battery.power_plugged
+        return True
+
+    def get_battery(self):
+        return (self.percent, self.charging)
+
+class Battery(Box):
+    def __init__(
+        self,
+    ):
+        self.provider = BatteryProvider()
+
+        super().__init__(
+            name="battery",
+            spacing=0,
+            visible=True,
+            all_visible=True,
+        )
+
+        self.batt_fabricator = Fabricator(
+            poll_from=lambda v: self.provider.get_battery(),
+            on_changed=lambda f, v: self.update_battery,
+            interval=1000,
+            stream=False,
+            default_value=0,
+        )
+        self.batt_fabricator.changed.connect(self.update_battery)
+        GLib.idle_add(self.update_battery, None, self.provider.get_battery())
+
+        self.percent_label = Label(label = "NULL")
+        self.children = [self.percent_label]
+
+    def _format_percentage(self, value: int) -> str:
+        return f"{value}%"
+    
+    def _update(self):
+        value, _ = self.provider.get_battery()
+        self.percent_label.set_label(self._format_percentage(int(value)))
+
+    def update_battery(self, sender, battery_data):
+        value, charging = battery_data
+        if value == 0:
+            self.set_visible = False
+        self.percent_label.set_label(self._format_percentage(int(value)))
 
 class StatusBar(Window):
     def __init__(
@@ -24,18 +90,20 @@ class StatusBar(Window):
             name="bar",
             layer="top",
             anchor="left top right",
-            # margin="10px 10px -2px 10px",
             exclusivity="auto",
             visible=True,
             all_visible=True,
         )
+
+        self.active_window = ActiveWindow(name="hyprland-window")
 
         self.workspaces = Workspaces(
             name="workspaces",
             spacing=4,
             buttons_factory=lambda ws_id: WorkspaceButton(id=ws_id, label=None),
         )
-        self.active_window = ActiveWindow(name="hyprland-window")
+
+        self.battery = Battery()
         self.date_time = DateTime(formatters="%a, %b %-d  %-I:%M %p", name="date-time")
 
         self.children = CenterBox(
@@ -58,6 +126,7 @@ class StatusBar(Window):
                 spacing=4,
                 orientation="h",
                 children=[
+                    self.battery,
                     self.date_time,
                 ],
             ),
